43615080201fed20400ef1372c7f27aa15615fc1
ademar <junioor.aguia@gmail.com>
2025-03-04 21:50:09 -0300

    evitando efeito Glitch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { AppRoutes } from "./app.routes"
+import { View } from "react-native"
+import { useTheme } from "styled-components/native"
 
+// aplicado view para evitar efeito de Glitch(Piscada) na navegação
+// acontece esse Glitch pq o fundo na tela está branco!
 
 export function Routes() {
+  const { COLORS } = useTheme()
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
 
   )
 }
